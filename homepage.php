27f6952d1d8ab10f27924a21b66789fb8e3360ba
m3ruem <109645090+m3ruem@users.noreply.github.com>
2024-06-25 15:03:51 +0800

    3:03 PM

--- homepage.php
+++ homepage.php
@@ -6,37 +6,43 @@ session_start()
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landing page</title>
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-    
-        <div class="w3-mobile w3-sidebar w3-bar-block w3-light-white w3-rightbar" style="height:100vh; display: flex; flex-direction:column;">
-
-            <div class="w3-mobile w3-panel w3-border-bottom w3-border-black"><img src="Eulap-black.png" alt="Eulap image" style="height:auto; width:70%; margin-right:auto; margin-left:15%"></div>
-
-            <a href="#" class="w3-bar-item w3-button w3-panel w3-border w3-hover-border-red"><strong>Contestant 1</strong></a>
-            <a href="#" class="w3-bar-item w3-button w3-panel w3-border w3-hover-border-red"><strong>Contestant 2</strong></a>
-            <a href="#" class="w3-bar-item w3-button w3-panel w3-border w3-hover-border-red"><strong>Contestant 3</strong></a>
-            <a href="#" class="w3-bar-item w3-button w3-panel w3-border w3-hover-border-red"><strong>Contestant 4</strong></a>
-            <a href="#" class="w3-bar-item w3-button w3-panel w3-border w3-hover-border-red"><strong>Contestant 5</strong></a>
-            <a href="#" class="w3-bar-item w3-button w3-panel w3-border w3-hover-border-red"><strong>Contestant 6</strong></a>
-
-            <!-- <div class="spacebox" style="margin-top:0%"></div> -->
-            <div class="" style="margin-top:auto">
-                <a href="">
-                    <button class="w3-button w3-white w3-border w3-border-black" style="width:100%; height:100%"><strong>LOGOUT</strong></button>
-                </a>
-            </div>
+    <div class="sidebar">
+        <div class="panel">
+            <img src="Eulap-black.png" alt="Eulap image">
         </div>
-    
-    
-</body>
 
+        <a href="#" class="panel">
+            <strong>Contestant 1</strong>
+        </a>
+        <a href="#" class="panel">
+            <strong>Contestant 2</strong>
+        </a>
+        <a href="#" class="panel">
+            <strong>Contestant 3</strong>
+        </a>
+        <a href="#" class="panel">
+            <strong>Contestant 4</strong>
+        </a>
+        <a href="#" class="panel">
+            <strong>Contestant 5</strong>
+        </a>
+        <a href="#" class="panel">
+            <strong>Contestant 6</strong>
+        </a>
+
+        <div class="logout-button">
+            <a href="">
+                <button><strong>LOGOUT</strong></button>
+            </a>
+        </div>
+    </div>
+</body>
 </html>
